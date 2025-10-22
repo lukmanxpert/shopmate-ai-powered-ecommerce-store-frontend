9da1bcbf0e52a98a76b0af1d15b9afa98f4217cd
lukmanxpert <lukmanmiah2004@gmail.com>
2025-10-22 17:17:18 +0600

    added review container func only

--- src/components/Products/ReviewsContainer.jsx
+++ src/components/Products/ReviewsContainer.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { postReview } from "../../store/slices/productSlice";
 
 const ReviewsContainer = ({ product, productReviews }) => {
+  const { authUser } = useSelector(state => state.auth)
+  const { isReviewDeleting, isPostingReview } = useSelector(state => state.product)
+  const dispatch = useDispatch()
+  const [rating, setRating] = useState(1)
+  const [comment, setComment] = useState("")
+  const handleReviewSubmit = (e) => {
+    e.preventDefault()
+    const data = new FormData()
+    data.append("rating", rating)
+    data.append("comment", comment)
+    dispatch(postReview({ productId: product.id, review: data }))
+  }
   return <></>;
 };
 

b34ce8cd34e93954225ed812a6056162ed5fa65f
lukmanxpert <lukmanmiah2004@gmail.com>
2025-10-24 14:31:10 +0600

    added products page func

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,8 +5,42 @@ import Pagination from "../components/Products/Pagination";
 import AISearchModal from "../components/Products/AISearchModal";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchAllProducts } from "../store/slices/productSlice";
 
 const Products = () => {
+  const { products, totalProducts } = useSelector(state => state.product)
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search)
+  }
+  const query = useQuery()
+  const searchTerm = query.get("search")
+  const searchedCategory = query.get("category")
+
+  const [searchQuery, setSearchQuery] = useState(searchTerm || "")
+  const [selectedCategory, setSelectedCategory] = useState(
+    searchedCategory || ""
+  )
+  const [priceRange, setPriceRange] = useState([0, 10000])
+  const [selectedRating, setSelectedRating] = useState(0)
+  const [availability, setAvailability] = useState("")
+  const [currentPage, setCurrentPage] = useState(1)
+  const [isMobileFilterOpen, setIsMobileFilterOpen] = useState(false)
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchAllProducts({
+      category: selectedCategory,
+      price: `${priceRange[0]}-${priceRange[1]}`,
+      search: searchQuery,
+      ratings: selectedRating,
+      availability: availability,
+      page: currentPage
+    }))
+  }, [dispatch, selectedCategory, priceRange, searchQuery, selectedRating, availability, currentPage])
+
+  const totalPages = Math.ceil(totalProducts / 10)
+
   return <></>;
 };
 
